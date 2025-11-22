b5fb9deba7c371a30dd403b77c811b04d00deae8
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-11-22 07:19:33 -0500

    added density arg for run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+DENSITY=0
 RESOURCE="resources/trophy.obj"
 if [ ! -z "$1" ]; then
     RESOURCE="$1"
 fi
 
+if [ ! -z "$2" ]; then
+    DENSITY="$2"
+fi
+
+
 ./build.sh
 
 echo "Running Application with default "$RESOURCE" "
-./build/bin/Debug/CloudPointRenderer.exe "$RESOURCE" || echo "Resource not found or improper"
+./build/bin/Debug/CloudPointRenderer.exe "$RESOURCE" "$DENSITY" || echo "Resource not found or improper"
 
